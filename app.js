620f0bd5a988d7430cb6d4d4f552dcf75a603944
Ryo <kanekuni0104@gmail.com>
2023-12-12 16:55:33 +0900

    css is activated

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 //view ejngineの設定
 app.set('view engine', 'ejs');
 //リクエスト内容の解析(これがないとinputの入力内容が見れない)
 app.use(bodyParser.urlencoded({extended: false}));
+
+//CSSまでのパスを有効にする
+app.use(express.static(path.join(__dirname, 'public')));        
 
 const homeRoutes = require('./routes/home');
 
