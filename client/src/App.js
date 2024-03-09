40a2a80c08cf5f3fa887e710d297357545a004b3
Ryo <kanekuni0104@gmail.com>
2024-03-09 22:09:57 +0900

    react講義の反映

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Archive from "./components/Archive";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/item" />
       </Routes>
     </Router>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,45 @@
 // Home.js
 
 import React from "react";
-import Bar from "./Home_bar";
-import Input from "./Home_explore"
 
 const Home = () => {
   return (
     <>
-      <Bar />
-      <Input />
+      <h1>Hello World!</h1>
+      <Nava  />
+      <InputContent />
     </>
   );
 };
+
+const Nava = () => {
+  return(
+    <div class="container-fluid">
+            <form action="/archive" method="GET">
+                <button class="btn btn-outline-primary" type="submit">アーカイブ</button>
+            </form>
+          <span class="navbar-toggler-icon"></span>
+    </div>
+  )
+};
+
+const InputContent = () => {
+  return(
+    <>
+      <div class="container">
+        <div class="row text-center pb-4">
+            <div class="col-md-8 order-1 ">
+                <form action="/item" method="POST">
+                    <input type="text" class="form-control" name="ToDoItem" placeholder="ToDoを入力"/>
+                </form>
+            </div>
+            <div class="col-md-2 order-2">
+              <button class="btn btn-primary" type="submit">追加</button> 
+            </div>
+        </div>
+      </div>    
+    </>
+  )
+}
 
 export default Home;
