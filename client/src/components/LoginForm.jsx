42400eaeb5f9e3683b4790a5cff98797c1edf6bb
Ryo <kanekuni0104@gmail.com>
2024-05-12 09:51:09 +0900

    firebaseでサインアップができるように実装した

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import {useNavigate}from 'react-router-dom';
-import API from '../api';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 /* MaterialUI */
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -16,17 +16,15 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const LoginForm = ({isSignup}) => {
-  const [input, setInput] = useState('');
-  const [inputUsername, setInputUsername] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+  const auth = getAuth();
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const handleChangeEmail = (event) => setInput(event.target.value);
-
-  const handleChangeUsername = (event) => setInputUsername(event.target.value);
+  const handleChangeEmail = (event) => setInputEmail(event.target.value);
 
   const handleChangePassword = (event) => setInputPassword(event.target.value);
   
@@ -36,62 +34,57 @@ const LoginForm = ({isSignup}) => {
   
   const submitUserInformation = (event) => {
     event.preventDefault();
-    const endPoint = isSignup ? '/auth/signup' : '/auth/login';
-    const data = isSignup ? {email: input, username: inputUsername, password: inputPassword} : {emailUsername: input, password: inputPassword}
-  
-    API.post(endPoint,data)
-      .then(() => {
-        console.log('送信に成功しました')
-        navigate('/')
-      })
-      .catch((err) => console.log('submitUserInformationのエラー:',err))
+    
+    if(isSignup) {
+      createUserWithEmailAndPassword(auth, inputEmail, inputPassword)
+        .then((userCredential) => {
+          const user = userCredential.inputEmail;
+          console.log('ユーザー登録に成功しました');
+        })
+        .catch((err) => console.log('firebaseでエラー発生',err));
+    }
   }
   return(
-      <Box
-          onSubmit={submitUserInformation}
-          component='form'
-          sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete='off'
-          >
-          <TextField id='standard-basic' label={isSignup ? 'Email' : 'Email or Username'} variant='standard' onChange={handleChangeEmail} required/>
-          {isSignup ?
-            <TextField label='Username' variant='standard' onChange={handleChangeUsername} required></TextField>
-          :
-          ''
-          }
-          <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
-              <InputLabel htmlFor='standard-adornment-password' required>
-                  Password
-              </InputLabel>
-              <Input
-                  id='standard-adornment-password'
-                  type={showPassword ? 'text' : 'password'}
-                  endAdornment={
-                  <InputAdornment position='end'>
-                      <IconButton
-                      aria-label='toggle password visibility'
-                      onClick={handleClickShowPassword}
-                      >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                  </InputAdornment>
-                  }
-                  onChange={handleChangePassword}
-                  required
-              />
-                <Button type='submit' color='primary' variant='contained' fullWidth>
-                {isSignup ? 'サインアップ' : 'サインイン'}
-                </Button>
-                {isSignup ? '' : <Button onClick={() => handleSignupClick()}>サインアップ</Button>}
-          </FormControl>
-      </Box>
+    <Box
+      onSubmit={submitUserInformation}
+      component='form'
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete='off'
+    >
+      <TextField id='standard-basic' label='Email' variant='standard' onChange={handleChangeEmail} required/>
+        <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
+          <InputLabel htmlFor='standard-adornment-password' required>
+            Password
+          </InputLabel>
+          <Input
+            id='standard-adornment-password'
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+            <InputAdornment position='end'>
+                <IconButton
+                aria-label='toggle password visibility'
+                onClick={handleClickShowPassword}
+                >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+            </InputAdornment>
+            }
+            onChange={handleChangePassword}
+            required
+          />
+          <Button type='submit' color='primary' variant='contained' fullWidth>
+          {isSignup ? 'サインアップ' : 'サインイン'}
+          </Button>
+          {isSignup ? '' : <Button onClick={() => handleSignupClick()}>サインアップ</Button>}
+        </FormControl>
+    </Box>
   )
 }
 
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,14 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-
+import { initializeApp } from 'firebase/app';
+ 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
+export default app;
